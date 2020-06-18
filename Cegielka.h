9850b0b4418991002180aa197fcbc4af65a05f01
pawel15342 <65711674+pawel15342@users.noreply.github.com>
2020-06-18 11:47:38 +0200

    Update Cegielka.h

--- Cegielka.h
+++ Cegielka.h
@@ -1,1 +1,28 @@
+#ifndef CEGIELKA_H
+#define CEGIELKA_H
+#include <SFML/Graphics.hpp>
+
+class Cegielka :
+        public sf::Drawable //deklaracja umożliwiajaca wykorzystanie sprite i tekstu
+{
+public:
+    Cegielka(float X, float Y, float szerokosc, float wysokosc, sf::Texture &texture); //deklaracja konstruktora pozycji, wymiarow i tekstury cegielki
+   ~Cegielka() {}
+    //ruch
+    float Lewo();
+    float Prawo();
+    float Gora();
+    float Dol();
+    //deklaracje funkcji rozbicia cegielki pilka
+    bool get_rozbij();
+    void set_zbij();
+    //wirtualna funkcja rysujaca cegielki
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates state) const;
+
+private:
+    sf::RectangleShape cegielka; //nadanie kszaltu celgielce
+    bool visible = false; //cegielka poczatkowo nie widoczna
+};
+
+#endif
 
