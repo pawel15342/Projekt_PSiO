604acc771149b6f211f9ea0f86c851a809704053
pawel15342 <65711674+pawel15342@users.noreply.github.com>
2020-06-18 11:50:13 +0200

    Update Pilka.h

--- Pilka.h
+++ Pilka.h
@@ -1,1 +1,42 @@
+#ifndef PILKA_H
+#define PILKA_H
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Odbijak.h"
+
+class Pilka : public sf::Drawable
+{
+public:
+    Pilka(float x, float y);
+   ~Pilka() {}
+
+    enum EtapGry { START, ROZGRYWKA }; // deklaracja etapow gry
+    EtapGry state;
+    //ruch
+    void update();
+    float Lewo();
+    float Prawo();
+    float Gora();
+    float Dol();
+
+//zmiana kierunku po odbiciu
+    void zmiana_y();
+    void zmiana_x();
+ //predkosc pilki
+    void set_predkosc(float X);
+    void set_pozycja(float x, float y);
+    float get_promien();
+
+    virtual void draw(sf::RenderTarget& cel, sf::RenderStates etap) const;
+
+private:
+
+    sf::CircleShape pilka; // nadanie kszatltu
+    sf::Texture texture; //deklaracja tekstury
+    const float ballPromien = 12.0f; //nadanie promienia pilce
+    float predkosc_pilki = 0.0f; //predkosc poczatkowa
+    sf::Vector2f predkosc = { predkosc_pilki, predkosc_pilki }; //predkosc w plaszczycie 2d
+};
+
+#endif
 
